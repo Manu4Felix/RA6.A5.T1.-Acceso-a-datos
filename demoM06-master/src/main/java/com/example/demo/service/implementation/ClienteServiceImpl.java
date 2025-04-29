a553c80b7fea57ed965677631e3b677fc0ba50da
Manu4Felix <manuel.felix.7e8@itb.cat>
2025-04-29 11:54:15 +0200

    3

--- demoM06-master/src/main/java/com/example/demo/service/implementation/ClienteServiceImpl.java
+++ demoM06-master/src/main/java/com/example/demo/service/implementation/ClienteServiceImpl.java
@@ -1,24 +1,62 @@
+package com.example.demo.service.implementation;
+
+import com.example.demo.persistence.dao.interfaces.IClienteDAO;
 import com.example.demo.persistence.entity.ClienteEntity;
 import com.example.demo.presentation.dto.ClienteDTO;
+import com.example.demo.service.interfaces.IClienteService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
-//@Override
-//public ClienteDTO updateCliente(ClienteDTO clienteDTO, Long id) {
-//   // Optional<ClienteEntity> clienteEntity = this.userDAO.findById(id);
-//
-//    if (clienteEntity.isPresent()) {
-//        ClienteEntity currentClienteEntity = clienteEntity.get();
-//        currentClienteEntity.setEmpresa(clienteDTO.getEmpresa());
-//        currentClienteEntity.setRepClie(clienteDTO.getRepClie());
-//        currentClienteEntity.setLimiteCredito(clienteDTO.getLimiteCredito());
-//
-//        this.userDAO.save(currentClienteEntity);
-//
-//        ModelMapper modelMapper = new ModelMapper();
-//        return modelMapper.map(currentClienteEntity, ClienteDTO.class);
-//    } else {
-//        throw new IllegalArgumentException("El cliente no existe");
-//    }
-//}
+@Service
+public class ClienteServiceImpl implements IClienteService {
+
+    @Autowired
+    private IClienteDAO clienteDAO;
+
+    @Override
+    public ClienteDTO updateCliente(ClienteDTO clienteDTO, Long id) {
+        Optional<ClienteEntity> clienteEntity = this.clienteDAO.findById(id);
+
+        if (clienteEntity.isPresent()) {
+            ClienteEntity currentClienteEntity = clienteEntity.get();
+            currentClienteEntity.setEmpresa(clienteDTO.getEmpresa());
+            currentClienteEntity.setRepClie(clienteDTO.getRepClie());
+            currentClienteEntity.setLimiteCredito(clienteDTO.getLimiteCredito());
+
+            this.clienteDAO.save(currentClienteEntity);
+
+            ModelMapper modelMapper = new ModelMapper();
+            return modelMapper.map(currentClienteEntity, ClienteDTO.class);
+        } else {
+            throw new IllegalArgumentException("El cliente no existe");
+        }
+    }
+
+    @Override
+    public List<ClienteDTO> findAll() {
+        // Implement other methods as needed
+        return null;
+    }
+
+    @Override
+    public ClienteDTO findById(Long id) {
+        // Implement other methods as needed
+        return null;
+    }
+
+    @Override
+    public ClienteDTO createCliente(ClienteDTO clienteDTO) {
+        // Implement other methods as needed
+        return null;
+    }
+
+    @Override
+    public String deleteCliente(Long id) {
+        // Implement other methods as needed
+        return null;
+    }
+}
--- demoM06-master/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demoM06-master/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.demo;
 
+import com.example.demo.persistence.entity.ClienteEntity;
+import com.example.demo.persistence.repository.IClienteRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
-class DemoApplicationTests {
+@Transactional
+class ClienteRepositoryIntegrationTest {
+
+	@Autowired
+	private IClienteRepository clienteRepository;
 
 	@Test
-	void contextLoads() {
-	}
+	void testSaveAndFindCliente() {
+		ClienteEntity cliente = ClienteEntity.builder()
+				.empresa("Empresa Test")
+				.repClie("Representante Test")
+				.limiteCredito(10000.0)
+				.build();
 
+		ClienteEntity savedCliente = clienteRepository.save(cliente);
+		Optional<ClienteEntity> foundCliente = clienteRepository.findById(savedCliente.getNum_clie());
+
+		assertTrue(foundCliente.isPresent());
+		assertEquals("Empresa Test", foundCliente.get().getEmpresa());
+	}
 }
